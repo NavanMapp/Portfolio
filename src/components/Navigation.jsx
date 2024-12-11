e3d8e02d3914427637ca49b303f0554f7fe91d07
NavanMapp <navanmapp@gmail.com>
2024-12-11 17:53:03 +0200

    css, component update

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,12 @@
 import { useRef } from "react"
 import Experience from "./Experience"
 import ExperienceCard from "./ExperienceCard"
+import projectImg from "../Images/projects.png"
+import educationImg from "../Images/education.png"
+import experienceImg from "../Images/experience.png"
+import aboutImg from "../Images/about.png"
+import emailImg from "../Images/gmail.png"
+import locationImg from "../Images/location.png"
 
 
 export default function Navigation({isOpen, toggleSidebar }) {
@@ -12,13 +18,13 @@ export default function Navigation({isOpen, toggleSidebar }) {
         const bioSection = document.querySelector(".bio-section")
         const projectSection = document.querySelector(".project-section")
 
-        if (experienceSection) {
+        if (projectSection) {
             experienceSection.scrollIntoView({ behavior: "smooth" })
         } else if (aboutSection) {
             aboutSection.scrollIntoView({ behavior: "smooth" })
         } else if (bioSection) {
             bioSection.scrollIntoView({ behavior: "smooth" })
-        } else if (projectSection) {
+        } else if (experienceSection ) {
             projectSection.scrollIntoView({ behavior: "smooth" })
         }
     }
@@ -32,37 +38,37 @@ export default function Navigation({isOpen, toggleSidebar }) {
                 <ul className="contact-info">
                     <h1 className="contact">Contact</h1>
                     <li>
-                        <img src="" alt="" className="icon email-icon" />
-                        <span>📧</span> navanmapp@gmail.com
+                        <img src={emailImg} alt="" className="icon email-icon" />
+                        <span></span> navanmapp@gmail.com
                     </li>
                     <li>
-                        <img src="" alt="" className="icon location-icon" />
-                        <span>📍</span> Gauteng, South Africa
+                        <img src={locationImg} alt="" className="icon location-icon" />
+                        <span></span> Gauteng, South Africa
                     </li>
                 </ul>
                 <div className="navigation-links">
                     <ul>
                         <li>
                             <a href="#projects" onClick={scrollToSection} >
-                                <img src="" alt="Projects Icon" className="icon projects-icon" />
+                                <img src={projectImg} alt="Projects Icon" className="icon projects-icon" />
                                 <span>Projects</span>
                             </a>
                         </li>
                         <li>
                             <a href="#experience" onClick={scrollToSection} >
-                                <img src="" alt="Experience Icon" className="icon experience-icon" />
+                                <img src={experienceImg} alt="Experience Icon" className="icon experience-icon" />
                                 <span>Work Experience</span>
                             </a>
                         </li>
                         <li>
                             <a href="#education" onClick={scrollToSection} >
-                                <img src="" alt="Education Icon" className="icon education-icon" />
+                                <img src={educationImg} alt="Education Icon" className="icon education-icon" />
                                 <span>Education</span>
                             </a>
                         </li>
                         <li>
                             <a href="#about" onClick={scrollToSection} >
-                                <img src="" alt="About Icon" className="icon about-icon" />
+                                <img src={aboutImg} alt="About Icon" className="icon about-icon" />
                                 <span>About</span>
                             </a>
                         </li>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,42 +1,76 @@
-import React from "react";
+import React from "react"
+import reactImg from "../Images/react.png"
+import jsImg from "../Images/js.png"
+import cssImg from "../Images/css.png"
+import firebaseImg from "../Images/database.png"
+import htmlImg from "../Images/html.png"
+import csharpImg from "../Images/csharp.png"
+import mysqlImg from "../Images/mysql.png"
+import uiImg from "../Images/ui.png"
 
-function Projects({title, description, image, refProp }) {
+function Projects({ title, description, image, refProp }) {
     return (
-        <div className="" style={{ padding: "20px", height: "100vh"}} >
+        <div className="project-section" style={{ padding: "20px", height: "100vh" }} >
             <h1>Projects</h1>
-            <section className="experienceCard">
-                { image && <img src={image} alt={title} className="card-image" /> }
-                <h2 className="card-title" >
-                    {title  }
-                    Mapp Supreme Website
-                </h2>
-                <h3>Ticket logging system for a local IT company.</h3>
-                <a href="https://mappsupreme-50938.web.app/" 
-                className="App-link" target="_blank"
-                rel="noopener noreferrer"
-                >
-                    Project Link
-                </a>
-                <ul>
-                    <a>React</a>
-                    <a>Javascript</a>
-                    <a>CSS</a>
-                    <a>Firebase</a>
-                </ul>
-            </section>
-            <section className="experienceCard">
-                <h2>
-                    <img className="" />
-                    Price Compare API
-                </h2>
-                <h3>Application that compares store products to others.</h3>
-                <ul>
-                    <a>C#, ASP.Net Core Web API</a>
-                    <a>React</a>
-                    <a>Material UI</a>
-                    <a>MySQL</a>
-                </ul>
-            </section>
+            <div className="project-info">
+                <section className="experience-card">
+                    {image && <img src={image} alt={title} className="card-image" />}
+                    <h2 className="card-title" >
+                        {title}
+                        Mapp Supreme Website
+                    </h2>
+                    <h3>Ticket logging system for a local IT company.</h3>
+                    <a href="https://mappsupreme-50938.web.app/" className="App-link" target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Project Link
+                    </a>
+                    <div className="stack">Stack</div>
+                    <ul className="" >
+                        <a href="https://www.javascript.com/">
+                            <img src={jsImg} alt="Javascript" className="icon" />Javascript
+                        </a>                        <a href="https://react.dev/">
+                            <img src={reactImg} alt="React" className="icon" />React
+                        </a>
+
+                        <a href="https://html.com" >
+                            <img src={htmlImg} alt="HTML" className="icon" />HTML
+                        </a>
+                        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" >
+                            <img src={cssImg} alt="CSS" className="icon" />CSS
+                        </a>
+                        <a href="https://firebase.google.com/" >
+                            <img src={firebaseImg} alt="Firebase" className="icon" />Firebase
+                        </a>
+                    </ul>
+                </section>
+                <section className="experience-card">
+                    <h2>
+                        <img className="" />
+                        Price Compare API
+                    </h2>
+                    <h3>Application that compares store products to others.</h3>
+                    <a href="" className="App-link" target="_blank" rel="noopener noreferrer">
+                        Project Link
+                    </a>
+                    <div className="stack">Stack</div>
+                    <ul>
+                        <a href="https://learn.microsoft.com/en-us/dotnet/csharp/">
+                            <img src={csharpImg} alt="C#, ASP.Net Core Web API" className="icon" />ASP.Net
+                        </a>
+                        <a href="https://react.dev/">
+                            <img src={reactImg} alt="React" className="icon" />React
+                        </a>
+                        <a href="https://mui.com" >
+                            <img src={uiImg} alt="Material UI" className="icon" />Material UI
+                        </a>
+                        <a href="https://mysql.com" >
+                            <img src={mysqlImg} alt="MySQL" className="icon" />MySQL
+                        </a>
+                    </ul>
+                </section>
+            </div>
+
         </div>
     )
 }
