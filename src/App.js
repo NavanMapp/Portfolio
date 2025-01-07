b2147416005fa9c5dda1d18ce433eee6ae119af9
NavanMapp <navanmapp@gmail.com>
2025-01-07 19:20:08 +0200

    Mobile css modification Auto scroll Navigation buttons edited Added Email & github links

--- src/App.js
+++ src/App.js
@@ -4,24 +4,49 @@ import Navigation from './components/Navigation'
 import Experience from './components/Experience'
 import Bio from './components/Bio'
 import Projects from './components/Projects'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import About from './components/About'
 
-function App () {
-  
+function App() {
+
   const [isSidebarOpen, setSidebarOpen] = useState(true)
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen)
+  }
+  
+  const bioRef = useRef(null)
+  const projectsRef = useRef(null)
+  const experienceRef = useRef(null)
+  const aboutRef = useRef(null)
+
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" })
   }
 
   return (
     <div className='App'>
       <div className={`main-content ${isSidebarOpen ? 'sidebar-open' : 'sidebar-closed'}`}>
-        <Navigation isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <Bio />
-        <Projects />
-        <Experience />
-        <About />
+        <Navigation isOpen={isSidebarOpen} toggleSidebar={toggleSidebar}
+          scrollToSection={scrollToSection}
+          sections={{
+            Bio: bioRef,
+            Projects: projectsRef,
+            Experience: experienceRef,
+            About: aboutRef
+          }}
+         />
+        <div ref={bioRef}>
+          <Bio />
+        </div>
+        <div ref={projectsRef}>
+          <Projects />
+        </div>
+        <div ref={experienceRef}>
+          <Experience />
+        </div>
+        <div ref={aboutRef}>
+          <About />
+        </div>
       </div>
     </div>
   )
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,67 +9,58 @@ import emailImg from "../Images/gmail.png"
 import locationImg from "../Images/location.png"
 import leftImg from "../Images/leftarrow.png"
 import rightImg from "../Images/rightarrow.png"
+import githubImg from "../Images/github.png"
 
 
-export default function Navigation({isOpen, toggleSidebar }) {
-
-    const scrollToSection = () => {
-        // e.preventDefault()
-        const experienceSection = document.querySelector(".experience-section")
-        const aboutSection = document.querySelector(".about-section")
-        const bioSection = document.querySelector(".bio-section")
-        const projectSection = document.querySelector(".project-section")
-
-        if (projectSection) {
-            experienceSection.scrollIntoView({ behavior: "smooth" })
-        } else if (aboutSection) {
-            aboutSection.scrollIntoView({ behavior: "smooth" })
-        } else if (bioSection) {
-            bioSection.scrollIntoView({ behavior: "smooth" })
-        } else if (experienceSection ) {
-            projectSection.scrollIntoView({ behavior: "smooth" })
-        }
-    }
+export default function Navigation({ isOpen, toggleSidebar, scrollToSection, sections }) {
 
     return (
         <div className={`sidebar ${isOpen ? "open" : "closed"}`} >
             <nav>
                 <a className="toggle-button" onClick={toggleSidebar} >
-                    {isOpen ? <img src={leftImg} className="icon" /> : <img src={rightImg} className="icon" /> }
+                    {isOpen ? <img src={leftImg} className="icon" /> : <img src={rightImg} className="icon" />}
                 </a>
                 <ul className="contact-info">
                     <h1 className="contact">Contact</h1>
                     <li>
-                        <img src={emailImg} alt="" className="icon email-icon" />
-                        <span className="label">navanmapp@gmail.com</span> 
+                        <a href="mailto:navanmapp@gmail.com">
+                            <img src={emailImg} alt="" className="icon email-icon" />
+                            <span className="label">Email Me</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/NavanMapp?tab=repositories">
+                            <img src={githubImg} className="icon github-icon" />
+                            <span className="label">Github</span>
+                        </a>
                     </li>
                     <li>
                         <img src={locationImg} alt="" className="icon location-icon" />
-                        <span className="label">Gauteng, South Africa</span> 
+                        <span className="label">Gauteng, South Africa</span>
                     </li>
                 </ul>
                 <div className="navigation-links">
                     <ul>
                         <li>
-                            <a href="#projects" onClick={scrollToSection} >
+                            <a href="#projects" onClick={() => scrollToSection(sections.Projects)} >
                                 <img src={projectImg} alt="Projects Icon" className="icon projects-icon" />
                                 <span className="label">Projects</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#experience" onClick={scrollToSection} >
+                            <a href="#experience" onClick={() => scrollToSection(sections.Experience)} >
                                 <img src={experienceImg} alt="Experience Icon" className="icon experience-icon" />
                                 <span className="label">Work Experience</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#education" onClick={scrollToSection} >
+                            <a href="#education" onClick={() => scrollToSection(sections.Education)} >
                                 <img src={educationImg} alt="Education Icon" className="icon education-icon" />
                                 <span className="label">Education</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#about" onClick={scrollToSection} >
+                            <a href="#about" onClick={() => scrollToSection(sections.About)} >
                                 <img src={aboutImg} alt="About Icon" className="icon about-icon" />
                                 <span className="label">About</span>
                             </a>
